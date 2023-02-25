5f1b889d4e9a2c41cca6f57f8e23ee3319056543
Dm94 <dm94@hotmail.es>
2023-02-25 11:11:42 +0100

    Added cors

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,23 @@
 import fastify from 'fastify';
 import config from './plugins/config.js';
 import routes from './routes/index.js';
+import cors from '@fastify/cors';
 
 const server = fastify({
   ajv: {
     customOptions: {
-      removeAdditional: "all",
+      removeAdditional: 'all',
       coerceTypes: true,
       useDefaults: true,
-    }
+    },
   },
   logger: {
     level: process.env.LOG_LEVEL,
   },
 });
-
+await server.register(cors, {
+  // put your options here
+});
 await server.register(config);
 await server.register(routes);
 await server.ready();
