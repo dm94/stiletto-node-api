e2b87798e4da25776ea8d5cb43a59b614e84f996
dm94 <dm94@hotmail.es>
2023-04-02 22:42:37 +0200

    Endpoint: linkServer

--- src/routes/walkers/auth/index.ts
+++ src/routes/walkers/auth/index.ts
@@ -1,10 +1,9 @@
 import { FastifyPluginAsync } from 'fastify';
 import { Type } from '@sinclair/typebox';
-import { WalkerInfo } from '@customtypes/walkers';
 import { GetDiscordServersRequest } from '@customtypes/requests/walkers';
 
 const routes: FastifyPluginAsync = async (server) => {
-  server.get<GetDiscordServersRequest, { Reply: WalkerInfo[] }>(
+  server.get<GetDiscordServersRequest>(
     '/',
     {
       onRequest: [server.authenticate],
@@ -53,6 +52,55 @@ const routes: FastifyPluginAsync = async (server) => {
 
       if (!request.query.redirect || !request.query.code) {
         return reply.code(400).send();
+      }
+
+      return reply.code(501).send({
+        message: 'This can only be done directly from the bot',
+      });
+    },
+  );
+  server.post(
+    '/',
+    {
+      onRequest: [server.authenticate],
+      schema: {
+        description: 'Link a discord server to the clan',
+        summary: 'linkServer',
+        operationId: 'linkServer',
+        tags: ['walkers'],
+        querystring: {
+          type: 'object',
+          required: ['discordserverid'],
+          properties: {
+            discordserverid: {
+              type: 'string',
+            },
+          },
+        },
+        security: [
+          {
+            token: [],
+          },
+        ],
+        response: {
+          202: Type.Object({
+            message: Type.String(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      if (!request?.dbuser) {
+        reply.code(401);
+        return new Error('Invalid token JWT');
+      }
+      if (!request?.dbuser.clanid) {
+        reply.code(405);
+        return new Error('No clan');
+      }
+      if (request?.dbuser.discordid === request?.dbuser.leaderid) {
+        reply.code(401);
+        return new Error('This action can only be performed by the leader');
       }
 
       return reply.code(501).send({
