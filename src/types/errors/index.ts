8c47c863cb80fae4156d9a61a947420f44320d21
Dm94 <dm94@hotmail.es>
2023-04-07 22:36:14 +0200

    Added dafault messages

--- src/types/errors/index.ts
+++ src/types/errors/index.ts
@@ -1,8 +1,21 @@
-import { Type } from '@sinclair/typebox';
+export const Error400Default = {
+  type: 'string',
+  description: 'Bad Request',
+};
+export const Error401Default = {
+  type: 'string',
+  description: 'Unauthorized',
+};
+export const Error404Default = {
+  type: 'string',
+  description: 'Not Found',
+};
+export const Error405Default = {
+  type: 'string',
+  description: 'Method Not Allowed',
+};
 
-export const Error400Default = Type.String();
-export const Error401Default = Type.String();
-export const Error404Default = Type.String();
-export const Error405Default = Type.String();
-
-export const Error503Default = Type.String();
+export const Error503Default = {
+  type: 'string',
+  description: 'Service Unavailable',
+};
