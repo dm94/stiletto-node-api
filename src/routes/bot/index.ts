d87b3e0f99e2fd7a9204b5428a0e43da1c855b0b
Dm94 <dm94@hotmail.es>
2023-04-07 19:42:40 +0200

    Endpoint: addTechByBot

--- src/routes/bot/index.ts
+++ src/routes/bot/index.ts
@@ -1,5 +1,11 @@
 import { GetWhoHasLearnRequest } from '@customtypes/requests/bot';
-import { TechUserInfo, TechUserSchema, Tree } from '@customtypes/techtree';
+import {
+  TechTreeInfo,
+  TechTreeSchema,
+  TechUserInfo,
+  TechUserSchema,
+  Tree,
+} from '@customtypes/techtree';
 import { Type } from '@sinclair/typebox';
 import { FastifyPluginAsync } from 'fastify';
 
@@ -79,6 +85,91 @@ const routes: FastifyPluginAsync = async (server) => {
             }
             if (err) {
               return reply.code(503).send();
+            }
+          },
+        );
+      } catch (err) {
+        console.log(err);
+        return reply.code(503).send();
+      }
+    },
+  );
+  server.post<GetWhoHasLearnRequest, { Reply: TechTreeInfo }>(
+    '/:discordid/tech',
+    {
+      onRequest: [server.botAuth],
+      schema: {
+        description: 'Adds the list of learned technologies to that user.',
+        summary: 'addTechByBot',
+        operationId: 'addTechByBot',
+        tags: ['bot', 'tech'],
+        params: {
+          type: 'object',
+          properties: {
+            discordid: { type: 'string' },
+          },
+        },
+        querystring: {
+          type: 'object',
+          required: ['tree', 'tech'],
+          properties: {
+            tech: {
+              type: 'string',
+              description: 'Item name',
+            },
+            tree: {
+              type: 'string',
+              enum: Object.values(Tree),
+              description: 'Technology tree of the item',
+            },
+          },
+        },
+        security: [
+          {
+            apiKey: [],
+          },
+        ],
+        response: {
+          200: TechTreeSchema,
+        },
+      },
+    },
+    (request, reply) => {
+      if (!request?.params?.discordid) {
+        return reply.code(400).send();
+      }
+
+      const tree: string = request.query?.tree ? request.query.tree : 'Vitamins';
+      const tech: string = request.query?.tech ? request.query.tech : 'Desert Mule';
+
+      try {
+        server.mysql.query(
+          'select discordTag from users where discordID=?',
+          [request.params.discordid],
+          (e, rows) => {
+            if (rows && rows[0] && rows[0].discordTag) {
+              const discordtag = rows[0].discordTag;
+
+              const techCollection = server.mongo.client.db('lastoasis').collection('tech');
+              techCollection
+                .updateOne(
+                  { discordtag: discordtag },
+                  { $addToSet: { [tree]: tech } },
+                  { upsert: true },
+                )
+                .then(() => {
+                  techCollection.findOne({ discordtag: discordtag }).then((techTree) => {
+                    if (techTree) {
+                      return reply.code(200).send(techTree);
+                    } else {
+                      return reply.code(201).send();
+                    }
+                  });
+                });
+            } else if (e) {
+              return reply.code(503).send();
+            } else {
+              return reply.code(404).send();
             }
           },
         );
