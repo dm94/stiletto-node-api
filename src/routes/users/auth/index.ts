7ef51d546f6a064c9b7b7cbe51726edf57bcc3bc
Dm94 <dm94@hotmail.es>
2023-04-15 17:44:33 +0200

    Fixed the firefox login

--- src/routes/users/auth/index.ts
+++ src/routes/users/auth/index.ts
@@ -30,59 +30,57 @@ const routes: FastifyPluginAsync = async (server) => {
       },
     },
     async (request, reply) => {
-      try {
-        const accessToken = await getAccessToken(request.query.code);
+      const accessToken = await getAccessToken(request.query.code);
 
-        if (accessToken && accessToken.access_token) {
-          const user = await getUser(accessToken.access_token);
+      if (accessToken && accessToken.access_token) {
+        const user = await getUser(accessToken.access_token);
 
-          if (user) {
-            const username = `${user.username}#${user.discriminator}`;
-            const discordId = user.id;
+        if (user) {
+          const username = `${user.username}#${user.discriminator}`;
+          const discordId = user.id;
 
-            if (discordId) {
-              const token = await reply.jwtSign({ discordid: discordId }, { expiresIn: '30d' });
-              server.mysql.query(
-                'select users.nickname, users.discordtag, users.discordID discordid, users.clanid, users.token from users where users.discordID=?',
-                discordId,
-                (err, result) => {
-                  if (result && result[0]?.token) {
-                    try {
-                      const valid = server.jwt.verify(result[0].token);
-                      if (valid) {
-                        return reply.code(202).send({
-                          discordid: discordId,
-                          token: result[0].token,
-                        });
-                      }
-                    } catch (e) {
-                      console.log('The token is malformed.', discordId);
+          if (discordId) {
+            const token = await reply.jwtSign({ discordid: discordId }, { expiresIn: '30d' });
+
+            /*
+            server.mysql.query(
+              'select users.nickname, users.discordtag, users.discordID discordid, users.clanid, users.token from users where users.discordID=?',
+              discordId,
+              (err, result) => {
+                if (result && result[0]?.token) {
+                  try {
+                    const valid = server.jwt.verify(result[0].token);
+                    if (valid) {
+                      return reply.code(202).send({
+                        discordid: discordId,
+                        token: result[0].token,
+                      });
                     }
+                  } catch (e) {
+                    console.log('The token is malformed.', discordId);
                   }
+                }
+              },
+            );
+            */
 
-                  const date = new Date().toISOString().split('T')[0];
-                  server.mysql.query(
-                    'INSERT INTO users(discordID, discordTag,token,createdAt) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE token=?, lastUpdate=?',
-                    [discordId, username, token, date, token, date],
-                  );
+            const date = new Date().toISOString().split('T')[0];
+            server.mysql.query(
+              'INSERT INTO users(discordID, discordTag,token,createdAt) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE token=?, lastUpdate=?',
+              [discordId, username, token, date, token, date],
+            );
 
-                  return reply.code(202).send({
-                    discordid: discordId,
-                    token: token,
-                  });
-                },
-              );
-            } else {
-              return reply.code(400).send();
-            }
+            return reply.code(202).send({
+              discordid: discordId,
+              token: token,
+            });
           } else {
             return reply.code(400).send();
           }
         } else {
           return reply.code(400).send();
         }
-      } catch (e) {
-        console.log('Auth Error', e);
+      } else {
         return reply.code(500).send();
       }
     },
