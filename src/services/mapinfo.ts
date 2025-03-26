2bcfd98c6295f80cdc7b236b95cddfe0ba5e1cec
Daniel Martín <dm94@hotmail.es>
2025-03-26 22:49:07 +0100

    fix(mapinfo): add optional chaining to prevent undefined access
    The changes add optional chaining to `req.query` and `req.body` to prevent potential runtime errors when accessing properties on undefined objects. This improves code robustness by ensuring safe access to nested properties.

--- src/services/mapinfo.ts
+++ src/services/mapinfo.ts
@@ -1,8 +1,8 @@
 import type { MapInfo } from '@customtypes/maps';
 
 export const addMapInfo = (server, req, done) => {
-  if (req.params.mapid && (req.query.mappass || req.body.mappass)) {
-    const pass = req.query.mappass ?? req.body.mappass;
+  if (req.params.mapid && (req?.query?.mappass || req?.body?.mappass)) {
+    const pass = req?.query?.mappass ?? req?.body?.mappass;
 
     server.mysql.query(
       'select mapid, typemap, discordID as discordid, name, dateofburning, pass, allowedit from clanmaps where mapid=? and pass=?',
